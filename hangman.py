1d6bdf378e9bedfc624fd4fbdc4214430bb5361e
l33th <l33th@protonmail.ch>
2021-12-03 19:11:52 +0200

    added play function

--- hangman.py
+++ hangman.py
@@ -4,3 +4,37 @@ from words import word_list
 def get_word():
 	word = random.choice(word_list)
 	return word.upper()
+
+def play(word):
+	word_completion = "_" * len(word)
+	guessed = False
+	guessed_letters = []
+	guessed_words = []
+	tries = 6
+
+	print("Let's play Hangman!")
+	print(display_hangman(tries))
+	print(word_completion)
+	print("\n")
+
+	while not guessed and tries > 0:
+		guess = input("Please guess a letter or word").upper()
+		if len(guess) == 1 and guess.isalpha():
+			if guess in guessed_letters:
+				print("You already guessed the letter", guess)
+			elif guess not in word:
+				print(guess, "is not in the word.")
+				tries -= 1
+				guessed_letters.append(guess)
+			else:
+				print("Good job,", guess, "is in the word!")
+				guessed_letters.append(guess)
+				word_as_list = list(word_completion)
+				indices = [i for i, letter in enumerate(word)]
+		elif len(guess) == len(word) and guess.isalpha():
+
+		else:
+			print("Not a valid guess.")
+		print(display_hangman(tries))
+		print(word_completion)
+		print("\n")
