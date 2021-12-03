ff72cb31e6426a818f2a5084fda7893b7c5f7d49
l33th <l33th@protonmail.ch>
2021-12-03 19:12:45 +0200

    added display_hangman function

--- hangman.py
+++ hangman.py
@@ -30,11 +30,104 @@ def play(word):
 				print("Good job,", guess, "is in the word!")
 				guessed_letters.append(guess)
 				word_as_list = list(word_completion)
-				indices = [i for i, letter in enumerate(word)]
-		elif len(guess) == len(word) and guess.isalpha():
+				indices = [i for i, letter in enumerate(word) if letter == guess]
+				for index in indices:
+					word_as_list[index] = guess
+					word_completion = "".join(word_as_list)
+					if "_" not in word_completion:
+                    guessed = True
+        elif len(guess) == len(word) and guess.isalpha():
+            if guess in guessed_words:
+                print("You already guessed the word", guess)
+            elif guess != word:
+                print(guess, "is not the word.")
+                tries -= 1
+                guessed_words.append(guess)
+            else:
+                guessed = True
+                word_completion = word
 
 		else:
 			print("Not a valid guess.")
 		print(display_hangman(tries))
 		print(word_completion)
 		print("\n")
+
+	if guessed:
+        print("Congrats, you guessed the word! You win!")
+    else:
+        print("Sorry, you ran out of tries. The word was " + word + ". Maybe next time!")
+
+def display_hangman(tries):
+    stages = [  # final state: head, torso, both arms, and both legs
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / \\
+                   -
+                """,
+                # head, torso, both arms, and one leg
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / 
+                   -
+                """,
+                # head, torso, and both arms
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |      
+                   -
+                """,
+                # head, torso, and one arm
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|
+                   |      |
+                   |     
+                   -
+                """,
+                # head and torso
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                """,
+                # head
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                """,
+                # initial empty state
+                """
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                """
+    ]
+    return stages[tries]
